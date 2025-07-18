ac1b7271f702b85f5bdab289563403ebebcb9e9a
aniketsawant50 <aniketsawant016@gmail.com>
2025-07-18 04:15:11 -0700

    Add files via upload

---
+++ onezeropattern.java
@@ -0,0 +1,15 @@
+public class onezeropattern {
+    public static void main(String args[])
+    {
+        int n = 5;
+        for(int i=1; i<=n; i++) {
+            int num = 1;
+            for(int j=1; j<=i; j++) {
+                System.out.print(num + " ");
+                num = 1 - num;
+            }
+            System.out.println();
+        }
+    }
+    
+}
